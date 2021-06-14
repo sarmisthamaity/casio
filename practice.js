2b5ef802288072e9a39d8932130636568dcb9d7e
sarmistha <64093543+sarmisthamaity@users.noreply.github.com>
2021-06-14 16:57:58 +0530

    Delete practice.js

--- practice.js
+++
@@ -1,9 +0,0 @@
-const WHITE_KEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-const BLACK_KEYS = ['n', 'o', 'p', 'q', 'r']
-key = 0
-const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-const blackKeyIndex = BLACK_KEYS.indexOf(key)
-console.log(whiteKeyIndex)
-console.log(blackKeyIndex)
-
-
