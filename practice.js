435b613ffc7ed408ad9e63ef3bb08056de6834b3
sarmistha <64093543+sarmisthamaity@users.noreply.github.com>
2021-06-14 16:57:40 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,9 @@
+const WHITE_KEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+const BLACK_KEYS = ['n', 'o', 'p', 'q', 'r']
+key = 0
+const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+const blackKeyIndex = BLACK_KEYS.indexOf(key)
+console.log(whiteKeyIndex)
+console.log(blackKeyIndex)
+
+
